0c3e6ad23601adca25d770fd94cdeecfda7af706
kopinad <kopinad73@gmail.com>
2025-04-07 22:58:27 +0300

    1

--- siteuser/urls.py
+++ siteuser/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from siteuser.views import svyaz
+from siteuser.views import svyaz, form_submit
 
 app_name = 'user'
 
 urlpatterns = [
     path('svyaz/', svyaz, name='svyaz'),
+    path('submit-form/', form_submit, name='form_submit'),
 ]
